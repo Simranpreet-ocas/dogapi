69a7f86787ba7c75e2596d675906eb2bbcea0536
Simranpreet Kaur <sikaur@ocas.ca>
2024-11-12 10:13:02 -0500

    Add the required Models for Authentication

---
+++ src/DogApi/Endpoints/Authentication/Models/AuthRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace DogApi.Endpoints.Authentication.Models
+{
+    public class AuthRequest
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+    }
+}
